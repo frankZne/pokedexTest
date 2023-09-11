55ad75f8617c6f3b4069374c17366f1436531922
frankSemNexus <francisco@semnexus.com>
2023-09-11 15:32:27 -0600

    Initial Commit

---
+++ pokedex/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  pokedex
+//
+//  Created by francisco rafael aguilar lugo on 11/09/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
